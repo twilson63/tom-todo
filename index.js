f2f47c1e7f7d6ff0bea9e9ded0d8e6287c02aad3
Tom Wilson <tom@jackhq.com>
2017-06-08 11:34:20 -0400

    version 1

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+#!/usr/bin/env node
+
 // const args = process.argv.splice(2)
 // const cmd = args[0]
 // const rest = args.slice(1)
 const [a, b, cmd, ...rest] = process.argv
 // commands
 const help = require('./cmds/help')
+const add = require('./cmds/add')
+const ls = require('./cmds/ls')
+const check = require('./cmds/check')
+const rm = require('./cmds/rm')
 
 function handler(cmd) {
   switch(cmd.toLowerCase()) {
+    case 'rm':
+      return rm(rest)
+    case 'check':
+      return check(rest)
+    case 'ls':
+      return ls()
+    case 'add':
+      return add(rest)
     case 'help':
       return help()
     default:
