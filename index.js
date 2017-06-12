1369cb7ff7e1e24b4f0cdc43172a729b8518501c
Tom Wilson <tom@jackhq.com>
2017-06-12 13:23:47 -0400

    new json

--- index.js
+++ index.js
@@ -3,21 +3,21 @@
 const [a, b, cmd, ...rest] = process.argv
 // commands
 const help = require('./cmds/help')
-const add = require('./cmds/add')
-const ls = require('./cmds/ls')
-const check = require('./cmds/check')
-// const rm = require('./cmds/rm')
+// const add = require('./cmds/add')
+// const ls = require('./cmds/ls')
+// const check = require('./cmds/check')
+//const rm = require('./cmds/rm')
 
 function handler(cmd = '') {
   switch (cmd) {
-    case 'rm':
-      return rm(rest)
-    case 'check':
-      return check(rest)
-    case 'ls':
-      return ls()
-    case 'add':
-      return add(rest)
+    // case 'rm':
+    //   return rm(rest)
+    // case 'check':
+    //   return check(rest)
+    // case 'ls':
+    //   return ls()
+    // case 'add':
+    //   return add(rest)
     case 'help':
       return help()
     default:
